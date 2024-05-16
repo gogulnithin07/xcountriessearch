224c2362d1d83ab4bb942a14216a53a420a7e5e1
martine9696 <gogulnithin07>
2024-05-16 21:54:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [input, setInput] = useState("");
+  const [filterArr, setFilterArr] = useState([]);
+  console.log("gogul".includes(""));
+  useEffect(
+    function () {
+      let fil = countries.slice().filter((val) => {
+        return val.name.common.toLowerCase().includes(input);
+      });
+      setFilterArr(fil);
+    },
+    [input]
+  );
+  useEffect(function () {
+    async function getData() {
+      try {
+        let req = await fetch("https://restcountries.com/v3.1/all");
+        if (!req.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        let res = await req.json();
+        setCountries(res);
+        setFilterArr(res);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value.toLowerCase())}
+      />
+      <div className="container">
+        {filterArr.map((val) => (
+          <Country
+            key={crypto.randomUUID()}
+            name={val.name.common}
+            flag={val.flags.png}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
+function Country({ name, flag }) {
+  return (
+    <div className="countryCard">
+      <img src={flag} alt="flagimage" />
+      <p>{name}</p>
+    </div>
+  );
+}
 export default App;
